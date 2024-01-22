142580e248a1898508502e5d7546e66634212913
philae-ael <1511446+philae-ael@users.noreply.github>
2024-01-22 18:02:29 +0100

    improve assertion handling

--- ToyRenderer/src/main.cpp
+++ ToyRenderer/src/main.cpp
@@ -5,11 +5,20 @@
 #include "app.h"
 #include "options.h"
 #include "registry.h"
+#include "utils/assert.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-auto main(int argc, const char *argv[]) -> int {
+[[noreturn]] void assert_violation_handler(const char* expr, const std::string& s, std::source_location loc) {
+  spdlog::critical("assertion failed: {}", expr);
+  spdlog::critical(s);
+  spdlog::info("assertion occured in {}:{} in {}", loc.function_name(), loc.line(), loc.file_name());
+
+  std::abort();
+}
+
+auto main(int argc, const char* argv[]) -> int {
   tr::Registry::load();
 
   const tr::Options args = tr::Options::from_args(std::span(argv, static_cast<std::size_t>(argc)));
--- utils/include/utils/assert.h
+++ utils/include/utils/assert.h
@@ -1,65 +1,12 @@
 #pragma once
 
-#include <spdlog/common.h>
-#include <spdlog/spdlog.h>
-
+#include <format>
 #include <source_location>
 
-#define FORMAT_STRING_EXPOSED __cpp_lib_format >= 202207L || __GLIBCXX__ >= 20230801
-#define SPDLOG_KNOWS_FORMAT_STRING_EXPOSED __cpp_lib_format >= 202207L
-
-#if FORMAT_STRING_EXPOSED
-#include <format>
-#else
-#include <string_view>
-#endif
+[[noreturn]] void assert_violation_handler(const char* expr, const std::string& s,
+                                           std::source_location loc = std::source_location::current());
 
 #define TR_ASSERT(expr, format, ...) TR_ASSERT_INNER(expr, #expr, format, __VA_ARGS__)
 
-#define TR_ASSERT_INNER(expr, expr_str, format, ...) \
-  static_cast<bool>(expr) ? void(0) : utils::details::logger{}.fail(expr_str, format __VA_OPT__(, ) __VA_ARGS__)
-
-namespace utils::details {
-
-// gcc supports exposing format_string but does not set __cpp_lib_format for 202207L
-// Spdlog only check for __cpp_lib_format so does not use format_string.
-// I want compile time help from the compiler. That explains the mess that follows
-// (Or i could do a PR to spdlog and wait 3 months until i can use the fix)
-//
-// See
-// https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2021/p2508r0.html
-// gcc.gnu.org/onlinedocs/libstdc++/manual/status.html
-
-template <typename... Args>
-#if FORMAT_STRING_EXPOSED
-using format_string = std::format_string<Args...>;
-#else
-using format_string = std::string_view;
-#endif
-
-template <typename... Args>
-constexpr auto to_spdlog_format_string(format_string<Args...> fmt) {
-#if SPDLOG_KNOWS_FORMAT_STRING_EXPOSED
-  return fmt;
-#elif FORMAT_STRING_EXPOSED
-  return fmt.get();
-#else
-  return fmt;
-#endif
-}
-
-struct logger {
-  std::source_location loc;
-  explicit logger(const std::source_location loc_ = std::source_location::current()) : loc(loc_) {}
-
-  template <typename... Args>
-  [[noreturn]] void fail(const char *expr, format_string<Args...> fmt, Args &&...args) {
-    spdlog::critical("assertion failed: {}", expr);
-    spdlog::critical(to_spdlog_format_string<Args...>(fmt), std::forward<Args>(args)...);
-    spdlog::info("assertion occured in {}:{} in {}", loc.function_name(), loc.line(), loc.file_name());
-
-    std::abort();
-  }
-};
-
-}  // namespace utils::details
+#define TR_ASSERT_INNER(expr, expr_str, fmt, ...) \
+  static_cast<bool>(expr) ? void(0) : assert_violation_handler(expr_str, std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
